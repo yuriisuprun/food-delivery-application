6d94f86e034b26e211ff7a54aa2b15fb9b6ce41e
yurii-suprun <Yurii_Suprun@epam.com>
2020-01-24 16:57:35 +0200

    CustomerServiceTest class was added

--- src/main/java/com/fooddelivery/service/CustomerService.java
+++ src/main/java/com/fooddelivery/service/CustomerService.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
-import java.util.Optional;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
 
