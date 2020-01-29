33f336a980e3c4af4ba2d6d3375bd1343018eb6a
Yurii Suprun <iursuprun@gmail.com>
2020-01-29 23:20:24 +0200

    some changes in tests

--- src/test/java/com/fooddelivery/service/CustomerServiceTest.java
+++ src/test/java/com/fooddelivery/service/CustomerServiceTest.java
@@ -1,21 +1,37 @@
 package com.fooddelivery.service;
 
+import com.fooddelivery.repository.CustomerRepository;
 import com.fooddelivery.service.CustomerService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+//@ExtendWith(MockitoJUnitRunner.class)
 class CustomerServiceTest {
 
-	@Autowired
+	@InjectMocks
 	private CustomerService customerService;
+
+	@Mock
+	private CustomerRepository customerRepository;
 
 	@Test
 	void shouldReturnCustomerLastName() {
 		String lastName = customerService.getCustomerById(1).getLastName();
 		assertEquals("Carter", lastName);
 	}
+
+	/*@Test
+	public void should() {
+		locationService.save(location);
+		Mockito
+				.verify(locationRepository, Mockito.atLeastOnce())
+				.save(location);
+	}*/
 }
