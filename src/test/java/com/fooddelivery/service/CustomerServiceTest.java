a797f2258a2d5d62131b06ddd4b011c0ac7f161c
yurii-suprun <Yurii_Suprun@epam.com>
2020-01-30 16:11:08 +0200

    shouldFindCustomerById test was added

--- src/test/java/com/fooddelivery/service/CustomerServiceTest.java
+++ src/test/java/com/fooddelivery/service/CustomerServiceTest.java
@@ -9,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -22,23 +23,34 @@ public class CustomerServiceTest {
 	@Mock
 	private CustomerRepository customerRepository;
 
-	private Customer customer;
+	private Customer customer1;
+	private Customer customer2;
+	private Customer customer3;
 
 	@Before
 	public void setUp() {
-		customer = new Customer("Jimmy", "Carter");
+		customer1 = new Customer("Jimmy", "Carter");
+		customer1.setId(1);
+		customer2 = new Customer("Theodore", "Roosevelt");
+		customer2.setId(2);
+		customer3 = new Customer("Harry", "Truman");
+		customer3.setId(3);
 	}
 
-	/*@Test
-	void shouldReturnCustomerLastName() {
-		String lastName = customerService.getCustomerById(1).getLastName();
-		assertEquals("Carter", lastName);
-	}*/
+	@Test
+	public void shouldFindCustomerById() {
+		when(customerRepository.findById(3)).thenReturn(java.util.Optional.of(new Customer("Harry", "Truman")));
+
+		Customer customer = customerRepository.findById(3).get();
+
+		assertEquals("Harry", customer.getFirstName());
+		assertEquals("Truman", customer.getLastName());
+	}
 
 	@Test
 	public void shouldAddCustomer() {
-		when(customerRepository.save(customer)).thenReturn(any(Customer.class));
+		when(customerRepository.save(customer1)).thenReturn(any(Customer.class));
 		customerService.addCustomer("Jimmy", "Carter");
-		verify(customerRepository).save(customer);
+		verify(customerRepository).save(customer1);
 	}
 }
