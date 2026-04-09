aed7a8da02a5ffbcb46871ebda341a1f84ec6b9e
yurii-suprun <iursuprun@gmail.com>
2026-04-09 16:41:09 +0200

    Fix docker-compose.yml build

--- infra/docker/postgres/init/01_init.sql
+++ infra/docker/postgres/init/01_init.sql
@@ -1,24 +1,3 @@
--- Create app user + database once, using the container's superuser (POSTGRES_USER defaults to postgres).
-do
-$$
-begin
-  if not exists (select from pg_roles where rolname = 'smarttrip') then
-    create role smarttrip login password 'smarttrip';
-  end if;
-end
-$$;
-
--- Ensure database exists.
-do
-$$
-begin
-  if not exists (select from pg_database where datname = 'smarttrip') then
-    create database smarttrip owner smarttrip;
-  end if;
-end
-$$;
-
--- Extensions are DB-scoped; create in smarttrip.
-\connect smarttrip
+-- The DB/user are created by POSTGRES_* env vars in docker-compose on first init.
+-- Enable pgvector in the default DB (smarttrip).
 create extension if not exists vector;
-
