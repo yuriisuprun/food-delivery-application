58e1e9a940f5562e1138e62f6a45419029853360
yurii-suprun <iursuprun@gmail.com>
2026-04-11 12:11:27 +0200

    Update ai service application

--- services/ai-service/src/main/java/com/smarttrip/aiservice/AiServiceApplication.java
+++ services/ai-service/src/main/java/com/smarttrip/aiservice/AiServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AiServiceApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(AiServiceApplication.class, args);
     }
