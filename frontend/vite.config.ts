f9617028a68b3431ccb1f8bdf25d7fe9b28866de
yurii-suprun <iursuprun@gmail.com>
2026-04-17 21:45:59 +0200

    Update frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 5173,
+  },
+});
+
