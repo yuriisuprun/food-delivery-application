920086b8522eb9ea3a917575d5a53c1dd2bfc285
yurii-suprun <iursuprun@gmail.com>
2026-04-21 22:55:17 +0200

    Update card.tsx

--- frontend/src/ui/components/ui/card.tsx
+++ frontend/src/ui/components/ui/card.tsx
@@ -5,14 +5,12 @@ const Card = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
+  <div ref={ref}
     className={cn(
       "rounded-xl border border-[color:var(--line)] bg-[color:var(--bg1)] backdrop-blur-sm transition-colors hover:bg-[color:var(--bg2)]",
       className
     )}
-    {...props}
-  />
+    {...props}/>
 ));
 Card.displayName = "Card";
 
@@ -28,11 +26,9 @@ const CardTitle = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLHeadingElement>
 >(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
+  <h3 ref={ref}
     className={cn("brand text-base sm:text-lg font-medium leading-none tracking-tight text-[color:var(--fg0)]", className)}
-    {...props}
-  />
+    {...props}/>
 ));
 CardTitle.displayName = "CardTitle";
 
@@ -40,11 +36,9 @@ const CardDescription = React.forwardRef<
   HTMLParagraphElement,
   React.HTMLAttributes<HTMLParagraphElement>
 >(({ className, ...props }, ref) => (
-  <p
-    ref={ref}
+  <p ref={ref}
     className={cn("text-xs sm:text-sm text-[color:var(--fg1)]", className)}
-    {...props}
-  />
+    {...props}/>
 ));
 CardDescription.displayName = "CardDescription";
 
@@ -60,11 +54,9 @@ const CardFooter = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div
-    ref={ref}
+  <div ref={ref}
     className={cn("flex items-center p-4 sm:p-5 pt-0", className)}
-    {...props}
-  />
+    {...props}/>
 ));
 CardFooter.displayName = "CardFooter";
 
